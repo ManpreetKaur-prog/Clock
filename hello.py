bad109faa20461d6ec36107eb9ec4bad97137228
ManpreetKaur-prog <72974411+ManpreetKaur-prog@users.noreply.github.com>
2021-06-10 21:54:26 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,19 @@
 Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> 
+from tkinter import*
+from tkinter.ttk import*
+
+from time import strftime
+
+root=Tk()
+root.title("clock"
+           def time():
+            string=strftime('%H: %M: %S %p')
+           label.config(text=string)
+           label.after(1000,time)
+label=Label(root,font=('Helvetica',80),bg="black",fg="cyan");
+label.pack(anchor='center')
+time()
+           
+mainloop()
